70997bdaf0a07d2465c6c45e3d74bfd778ee6002
Sena543 <senanuazanu@gmail.com>
2023-11-11 21:43:05 +0000

    formatting and import updates

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Dashboard from './Dashboard';
-import Login from './Login';
-import AudioTranscription from './AudioTranscription';
-import TranscribeAudio from './TranscribeAudio';
-import ProfileMenu from './ProfileMenu';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
+import AudioTranscription from "./pages/AudioTranscription";
+import TranscribeAudio from "./pages/TranscribeAudio";
+import ProfileMenu from "./ProfileMenu";
 
 const App = () => {
-  return (
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/profile" element={<ProfileMenu />} />
-        <Route path="/transcription/:actionNumber" element={<AudioTranscription />} />
-        <Route path="/transcribe/:audioId" element={<TranscribeAudio />} />
-       
-      </Routes>
-  );
+	return (
+		<Routes>
+			<Route path="/" element={<Login />} />
+			<Route path="/dashboard" element={<Dashboard />} />
+			<Route path="/profile" element={<ProfileMenu />} />
+			<Route path="/transcription/:actionNumber" element={<AudioTranscription />} />
+			<Route path="/transcribe/:audioId" element={<TranscribeAudio />} />
+		</Routes>
+	);
 };
 
 export default App;
